76e4cc3be4bdb90a4dad4fb698256334cef8aa10
siriusbks <nath.bordat@hotmail.fr>
2024-04-16 21:37:05 +0200

    Remove redundant reload command, add Metrics

--- src/main/java/fr/nbstudio/signwarp/SignWarp.java
+++ src/main/java/fr/nbstudio/signwarp/SignWarp.java
@@ -1,5 +1,6 @@
 package fr.nbstudio.signwarp;
 
+import fr.nbstudio.signwarp.bstats.Metrics;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.PluginManager;
@@ -8,18 +9,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class SignWarp extends JavaPlugin {
 
     public void onEnable() {
+        // Save default config
         saveDefaultConfig();
+
+        // Initialize bStats
+        int pluginId = 21626;
+        new Metrics(this, pluginId);
+
+        // Register commands and tab completer
         SWReloadCommand reloadCommand = new SWReloadCommand(this);
         PluginCommand command = getCommand("signwarp");
         if (command != null) {
             command.setExecutor(reloadCommand);
-            command.setTabCompleter((TabCompleter) reloadCommand);
         } else {
             getLogger().warning("Command 'signwarp' not found!");
         }
 
+        // Register event listener
         PluginManager pluginManager = getServer().getPluginManager();
-
         pluginManager.registerEvents(new EventListener(this), this);
 
     }
