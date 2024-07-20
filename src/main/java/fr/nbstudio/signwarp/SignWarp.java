12b16281b9e1509c8b7480a94ebdf52a7d534edd
siriusbks <nath.bordat@hotmail.fr>
2024-07-21 00:14:12 +0200

    Added update checker for plugin version

--- src/main/java/fr/nbstudio/signwarp/SignWarp.java
+++ src/main/java/fr/nbstudio/signwarp/SignWarp.java
@@ -7,7 +7,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SignWarp extends JavaPlugin {
 
+    private static final int RESOURCE_ID = 116195;
+    private static final String PLUGIN_URL = "https://www.spigotmc.org/resources/signwarp-teleport-using-the-signs." + RESOURCE_ID + "/";
+
     public void onEnable() {
+        // Check for updates
+        new UpdateChecker(this, RESOURCE_ID).getVersion(version -> {
+            if (this.getDescription().getVersion().equals(version)) {
+                getLogger().info("No new version available.");
+            } else {
+                getLogger().info("A new version of the plugin is available: " + version + " (current: " + this.getDescription().getVersion() + "). Download it here: " + PLUGIN_URL);
+            }
+        });
+
         // Save default config
         saveDefaultConfig();
 
