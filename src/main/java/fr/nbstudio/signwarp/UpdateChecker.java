03939b97fb1f2bc09bb9e8156648cbc3dd0a0938
Nathan <nath.bordat@hotmail.fr>
2025-09-08 19:23:52 +0200

    refactor: update getVersion method to use async scheduler for improved performance

--- src/main/java/fr/nbstudio/signwarp/UpdateChecker.java
+++ src/main/java/fr/nbstudio/signwarp/UpdateChecker.java
@@ -1,5 +1,6 @@
 package fr.nbstudio.signwarp;
 
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -20,14 +21,23 @@ public class UpdateChecker {
     }
 
     public void getVersion(final Consumer<String> consumer) {
-        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
-            try (InputStream is = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scann = new Scanner(is)) {
+        Bukkit.getAsyncScheduler().runNow(plugin, (ScheduledTask task) -> {
+            String latest = null;
+            try (InputStream is = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream();
+                 Scanner scann = new Scanner(is)) {
                 if (scann.hasNext()) {
-                    consumer.accept(scann.next());
+                    latest = scann.next();
                 }
             } catch (IOException e) {
                 plugin.getLogger().info("Unable to check for updates: " + e.getMessage());
             }
+
+            final String result = latest;
+            Bukkit.getGlobalRegionScheduler().run(plugin, t -> {
+                if (result != null) {
+                    consumer.accept(result);
+                }
+            });
         });
     }
 }
